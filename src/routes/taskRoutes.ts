1dc0d0a4743a420089b836a2fea4750616791731
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2025-08-25 13:03:36 +0530

    redis setup

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -8,7 +8,7 @@ router.use(authenticateToken)
 
 router.post('/create', taskController.createTask)
 router.post('/details', taskController.getTaskDetails)
-router.put('/:taskId', taskController.updateTask)
+router.put('/update/:taskId', taskController.updateTask)
 router.delete('/delete/:taskId', taskController.deleteTask)
 
 export default router
